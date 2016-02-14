7f984254664c16c158bc372ecb7ae21ecb2c5e23
Ahmet Arsan <aarsan@progger.com>
2016-02-14 15:36:28 -0600

    added stdout

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ end
 control_group '1 Verify All Folders Exist' do
 	control '1.1 Verify Ahmet directory exists' do
 		it 'test if folder is there' do
-			expect(command("(test-path '/ahmets')")).to match(True)
+			expect(command("(test-path '/ahmets')").stdout).to match(True)
 		end	
 	end
 end
